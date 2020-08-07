6ac664cb8bd985113daad13c89a9610d840a7355
SSsensei96 <steelseries175@gmail.com>
2020-08-07 18:56:42 +0600

    discord bot with 2 general fun and mod cogs

--- discord_natalie.py
+++ discord_natalie.py
@@ -1,22 +1,37 @@
+import os
+
 import discord
 
 from secret import token
+from discord.ext import commands
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+client = commands.Bot(command_prefix='!')
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_member_join(member):
+    channel = client.get_channel(741241273268043816)
+    await channel.send(f"{member} has joined a server")
 
-    if message.content:
-        await message.channel.send(message.content)
 
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(741241273268043816)
+    await channel.send(f"{member} has left a server")
+
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
 
 client.run(token)
