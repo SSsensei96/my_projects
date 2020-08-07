17f6502c92625c3958f9cb87150a5df3d19a5cb1
SSsensei96 <steelseries175@gmail.com>
2020-08-07 19:03:22 +0600

    mod cog for discord bot (reload command added) includes kick,ban,unban commands

--- discord_natalie.py
+++ discord_natalie.py
@@ -30,6 +30,12 @@ async def unload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
 
 
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.unload_extension(f"cogs.{extension}")
+
+
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
