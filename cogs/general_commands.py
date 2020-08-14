c21a13321bc11e6d6dba57d44272b0c42f184956
SSsensei96 <steelseries175@gmail.com>
2020-08-14 19:07:19 +0600

    added currency conversion async now

--- cogs/general_commands.py
+++ cogs/general_commands.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
 
-import ujson
+import asyncio
 
 import aiohttp
-
+import ujson
 from currency_convert import CurrencyConvertor
 from secret import weather_token
 
@@ -62,17 +62,29 @@ class GeneralCommands(commands.Cog):
         return city
 
     @commands.command()
-    async def convert(self, ctx):
-        url = str.__add__('http://data.fixer.io/api/latest?access_key=', "d387292f1ba52feb8e1055840e9aea59")
-        c = CurrencyConvertor(url)
+    async def convert(self, ctx, from_currency, to_currency, amount):
         message_to_split = ctx.message.content[8::]
         spit_message = message_to_split.split()
-        from_country = spit_message[0]
-        to_country = spit_message[1]
+        from_currency = spit_message[0]
+        to_currency = spit_message[1]
         initial_amount = int(spit_message[2])
-        res = c.convert(from_country, to_country, initial_amount)
-        result_message = f"{initial_amount} {from_country} = {res} {to_country}"
-        await ctx.send(result_message)
+        async with aiohttp.ClientSession(
+                json_serialize=ujson.dumps) as session:
+            url = str("http://data.fixer.io/api/latest?access_key=d387292f1ba52feb8e1055840e9aea59")
+            await session.post(url, json={'test': 'object'})
+            async with session.get(url) as resp:
+                data = await resp.json()
+
+            rates = data["rates"]
+
+            initial_amount = amount
+            if from_currency != 'EUR':
+                amount = int(amount) / rates[from_currency]
+
+            amount = round(amount * rates[to_currency], 2)
+            result_message = f"{initial_amount} {from_currency} = {amount} {to_currency}"
+            await ctx.send(result_message)
+
 
 
 
